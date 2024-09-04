7c9420ec99a7689d53d22c1de3833051653992e6
suriyas13 <suriyak1328@gmail.com>
2024-09-04 10:06:52 +0530

    Zombie Problem

---
+++ TCS/NQT/Zombiee/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the energy of Hero");
+        int hero = sc.nextInt();
+        System.out.println("Enter the no. of Zombies");
+        int n = sc.nextInt();
+        int[] z = new int[n];
+        System.out.println("Enter the energy of zombie");
+        for (int i = 0; i < n; i++) {
+            z[i] = sc.nextInt();
+        }
+        int flag = 1;
+        for (int i = 0; i < n; i++) {
+            if (hero < z[i]) {
+                flag = 0;
+                break;
+            } else {
+                hero -= ((z[i] % 2) + (z[i] / 2));
+            }
+        }
+        if (flag == 1) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
